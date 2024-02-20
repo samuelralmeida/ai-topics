61e6a42b2489f83c4a1373c290a63658d7a295b9
samuelralmeida <samuel.ralmeida@gmail.com>
2024-02-20 15:32:00 -0300

    add minimax algorithm

--- adversarial-search/main.go
+++ adversarial-search/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 	"log"
 
@@ -48,97 +49,113 @@ func (m *Measurements) Square() int {
 }
 
 type Game struct {
-	Board [][]string
+	Board           [][]string
+	BackgroundBoard [][]RectangleArea
 	Measurements
-	User   string
-	Player string
+	User            string
+	Player          string
+	UserStartButton RectangleArea
+	AiStartButton   RectangleArea
+	Title           string
+	PlayAgainButton RectangleArea
+	GameOver        bool
 }
 
 func (g *Game) Update() error {
 
+	// menu
 	if g.User == "" {
 		if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
 			mx, my := ebiten.CursorPosition()
 
-			if mx > g.Margin() && mx < g.Margin()+g.Space()*6 && my > g.Margin() && my < g.Margin()+g.Space()*2 {
+			if g.UserStartButton.IsPointInside(mx, my) {
 				g.User = "X"
-			} else if mx > g.Margin() && mx < g.Margin()+g.Space()*6 && my > g.Margin()+g.Space()*3 && my < g.Margin()+g.Space()*5 {
+			} else if g.AiStartButton.IsPointInside(mx, my) {
 				g.User = "O"
 			}
 		}
 		return nil
 	}
 
-	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
-		mx, my := ebiten.CursorPosition()
+	gameOver := terminal(g.Board)
+	player := player(g.Board)
 
-		rowIdx := -1
-		columnIdx := -1
-
-		if mx > g.Margin() && mx < g.Margin()+g.Square() && my > g.Margin() && my < g.Margin()+g.Square() {
-			rowIdx = 0
-			columnIdx = 0
-		} else if mx > g.Margin() && mx < g.Margin()+g.Square() && my > g.Margin()+g.Square() && my < g.Margin()+g.Square()*2 {
-			rowIdx = 1
-			columnIdx = 0
-		} else if mx > g.Margin() && mx < g.Margin()+g.Square() && my > g.Margin()+g.Square()*2 && my < g.Margin()+g.Square()*3 {
-			rowIdx = 2
-			columnIdx = 0
-		} else if mx > g.Margin()+g.Square() && mx < g.Margin()+g.Square()*2 && my > g.Margin() && my < g.Margin()+g.Square() {
-			rowIdx = 0
-			columnIdx = 1
-		} else if mx > g.Margin()+g.Square() && mx < g.Margin()+g.Square()*2 && my > g.Margin()+g.Square() && my < g.Margin()+g.Square()*2 {
-			rowIdx = 1
-			columnIdx = 1
-		} else if mx > g.Margin()+g.Square() && mx < g.Margin()+g.Square()*2 && my > g.Margin()+g.Square()*2 && my < g.Margin()+g.Square()*3 {
-			rowIdx = 2
-			columnIdx = 1
-		} else if mx > g.Margin()+g.Square()*2 && mx < g.Margin()+g.Square()*3 && my > g.Margin() && my < g.Margin()+g.Square() {
-			rowIdx = 0
-			columnIdx = 2
-		} else if mx > g.Margin()+g.Square()*2 && mx < g.Margin()+g.Square()*3 && my > g.Margin()+g.Square() && my < g.Margin()+g.Square()*2 {
-			rowIdx = 1
-			columnIdx = 2
-		} else if mx > g.Margin()+g.Square()*2 && mx < g.Margin()+g.Square()*3 && my > g.Margin()+g.Square()*2 && my < g.Margin()+g.Square()*3 {
-			rowIdx = 2
-			columnIdx = 2
+	if gameOver {
+		g.GameOver = true
+		winner := winner(g.Board)
+		if winner == "" {
+			g.Title = "Game over: Tie."
+		} else {
+			g.Title = fmt.Sprintf("Game over: %s wins.", winner)
 		}
 
-		if rowIdx == -1 || columnIdx == -1 {
-			return nil
+		if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+			mx, my := ebiten.CursorPosition()
+			if g.PlayAgainButton.IsPointInside(mx, my) {
+				g.Board = initialState()
+				g.GameOver = false
+				g.User = ""
+			}
 		}
 
-		g.Board[rowIdx][columnIdx] = g.Player
-		g.ChangePlayer()
-
+		return nil
 	}
+
+	// user
+	if g.User == player {
+		g.Title = fmt.Sprintf("Play as %s", g.User)
+		if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+			mx, my := ebiten.CursorPosition()
+
+			rowIdx := -1
+			columnIdx := -1
+
+			for i, row := range g.BackgroundBoard {
+				for j, area := range row {
+					if area.IsPointInside(mx, my) {
+						rowIdx = i
+						columnIdx = j
+						break
+					}
+				}
+			}
+
+			if rowIdx == -1 || columnIdx == -1 {
+				return nil
+			}
+
+			if g.Board[rowIdx][columnIdx] == "" {
+				g.Board[rowIdx][columnIdx] = player
+			}
+		}
+		return nil
+	}
+
+	// ai
+	g.Title = "AI thinking..."
+	move := minimax(g.Board)
+	board := result(g.Board, move)
+	g.Board = board
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 
-	// var h float32 = 0
-	// for h <= float32(g.Measurements.Width) {
-	// 	h = h + 50
-	// 	vector.StrokeLine(screen, 0, h, float32(g.Measurements.Width), h, 3, color.RGBA{255, 0, 0, 0}, false)
-	// 	vector.StrokeLine(screen, h, 0, h, float32(g.Measurements.Width), 3, color.RGBA{255, 0, 0, 0}, false)
-	// }
-
 	if g.User == "" {
-		vector.StrokeLine(screen, float32(g.Margin()), float32(g.Margin()), float32(g.Margin()+g.Space()*6), float32(g.Margin()), 3, color.White, false)
-		vector.StrokeLine(screen, float32(g.Margin()), float32(g.Margin()), float32(g.Margin()), float32(g.Margin()+g.Space()*2), 3, color.White, false)
-		vector.StrokeLine(screen, float32(g.Margin()), float32(g.Margin()+g.Space()*2), float32(g.Margin()+g.Space()*6), float32(g.Margin()+g.Space()*2), 3, color.White, false)
-		vector.StrokeLine(screen, float32(g.Margin()+g.Space()*6), float32(g.Margin()), float32(g.Margin()+g.Space()*6), float32(g.Margin()+g.Space()*2), 3, color.White, false)
+		userButtonLines := g.UserStartButton.Lines()
+		for _, l := range userButtonLines {
+			vector.StrokeLine(screen, float32(l.X0), float32(l.Y0), float32(l.X1), float32(l.Y1), 3, color.White, false)
+		}
 
 		msg := "I START"
 		textWidth := font.MeasureString(bigText, msg).Ceil()
 		textX := (g.Width - textWidth) / 2
 		text.Draw(screen, msg, bigText, textX, g.Margin()+g.Space()+20, color.White)
 
-		vector.StrokeLine(screen, float32(g.Margin()), float32(g.Margin()+g.Space()*3), float32(g.Margin()+g.Space()*6), float32(g.Margin()+g.Space()*3), 3, color.White, false)
-		vector.StrokeLine(screen, float32(g.Margin()), float32(g.Margin()+g.Space()*5), float32(g.Margin()+g.Space()*6), float32(g.Margin()+g.Space()*5), 3, color.White, false)
-		vector.StrokeLine(screen, float32(g.Margin()), float32(g.Margin()+g.Space()*3), float32(g.Margin()), float32(g.Margin()+g.Space()*5), 3, color.White, false)
-		vector.StrokeLine(screen, float32(g.Margin()+g.Space()*6), float32(g.Margin()+g.Space()*3), float32(g.Margin()+g.Space()*6), float32(g.Margin()+g.Space()*5), 3, color.White, false)
+		aiButtonLines := g.AiStartButton.Lines()
+		for _, l := range aiButtonLines {
+			vector.StrokeLine(screen, float32(l.X0), float32(l.Y0), float32(l.X1), float32(l.Y1), 3, color.White, false)
+		}
 
 		msg = "AI STARTS"
 		textWidth = font.MeasureString(bigText, msg).Ceil()
@@ -148,12 +165,19 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		return
 	}
 
-	instruction := "Your turn"
-	if g.User != g.Player {
-		instruction = "Computer thinking..."
+	if g.GameOver {
+		// playAgainButton := g.PlayAgainButton.Lines()
+		// for _, l := range playAgainButton {
+		// 	vector.StrokeLine(screen, float32(l.X0), float32(l.Y0), float32(l.X1), float32(l.Y1), 3, color.White, false)
+		// }
+
+		msg := "PLAY AGAIN"
+		textWidth := font.MeasureString(bigText, msg).Ceil()
+		textX := (g.Width - textWidth) / 2
+		text.Draw(screen, msg, bigText, textX, g.Margin()+g.Space()*7+20, color.White)
 	}
 
-	text.Draw(screen, instruction, bigText, g.Space(), g.Space()*2, color.White)
+	text.Draw(screen, g.Title, bigText, g.Space(), g.Space()*2, color.White)
 
 	vector.StrokeLine(screen, float32(g.Margin()), float32(g.Margin()+g.Square()), float32(g.Margin()+g.Square()*3), float32(g.Measurements.Margin()+g.Measurements.Square()), 3, color.White, false)
 	vector.StrokeLine(screen, float32(g.Margin()), float32(g.Margin()+g.Square()*2), float32(g.Margin()+g.Square()*3), float32(g.Margin()+g.Square()*2), 3, color.White, false)
@@ -192,20 +216,116 @@ func main() {
 	ebiten.SetWindowSize(width, height)
 	ebiten.SetWindowTitle("Play Tic-Tac-Toe")
 
+	measurements := Measurements{
+		Width:  width,
+		Height: height,
+	}
+
+	userStartButton := NewRectangleArea(measurements.Space()*6, measurements.Space()*2, measurements.Margin(), measurements.Margin())
+	aiStartButton := NewRectangleArea(measurements.Space()*6, measurements.Space()*2, measurements.Margin(), measurements.Margin()+measurements.Space()*3)
+	playAgainButton := NewRectangleArea(measurements.Space()*6, measurements.Space()*2, measurements.Margin(), measurements.Margin()+measurements.Space()*6)
+
 	game := &Game{
 		Board: [][]string{
 			{"", "", ""},
 			{"", "", ""},
 			{"", "", ""},
 		},
-		Measurements: Measurements{
-			Width:  width,
-			Height: height,
-		},
-		User:   "",
-		Player: "X",
+		Measurements:    measurements,
+		User:            "",
+		Player:          "X",
+		UserStartButton: userStartButton,
+		AiStartButton:   aiStartButton,
+		PlayAgainButton: playAgainButton,
+		BackgroundBoard: NewBackgroundBoard(measurements.Square(), measurements.Square(), measurements.Margin(), measurements.Margin()),
 	}
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
+	}
+}
+
+type RectangleArea struct {
+	Width  int
+	Height int
+	X      int
+	Y      int
+}
+
+type Line struct {
+	X0 int
+	Y0 int
+	X1 int
+	Y1 int
+}
+
+func (ra *RectangleArea) TopHorizontalLine() Line {
+	return Line{
+		X0: ra.X,
+		Y0: ra.Y,
+		X1: ra.X + ra.Width,
+		Y1: ra.Y,
+	}
+}
+
+func (ra *RectangleArea) BottomHorizontalLine() Line {
+	return Line{
+		X0: ra.X,
+		Y0: ra.Y + ra.Height,
+		X1: ra.X + ra.Width,
+		Y1: ra.Y + ra.Height,
+	}
+}
+
+func (ra *RectangleArea) LeftVerticalLine() Line {
+	return Line{
+		X0: ra.X,
+		Y0: ra.Y,
+		X1: ra.X,
+		Y1: ra.Y + ra.Height,
+	}
+}
+
+func (ra *RectangleArea) RightVerticalLine() Line {
+	return Line{
+		X0: ra.X + ra.Width,
+		Y0: ra.Y,
+		X1: ra.X + ra.Width,
+		Y1: ra.Y + ra.Height,
+	}
+}
+
+func NewRectangleArea(width, height, relativeX, relativeY int) RectangleArea {
+	return RectangleArea{Width: width, Height: height, X: relativeX, Y: relativeY}
+}
+
+func (ra *RectangleArea) IsPointInside(x, y int) bool {
+	return x > ra.X && x < (ra.X+ra.Width) && y > ra.Y && y < (ra.Y+ra.Height)
+}
+
+func (ra *RectangleArea) Lines() []Line {
+	return []Line{
+		ra.TopHorizontalLine(),
+		ra.BottomHorizontalLine(),
+		ra.LeftVerticalLine(),
+		ra.RightVerticalLine(),
+	}
+}
+
+func NewBackgroundBoard(width, height, relativeX, relativeY int) [][]RectangleArea {
+
+	area00 := NewRectangleArea(width, height, relativeX, relativeY)
+	area01 := NewRectangleArea(width, height, relativeX+width, relativeY)
+	area02 := NewRectangleArea(width, height, relativeX+width*2, relativeY)
+	area10 := NewRectangleArea(width, height, relativeX, relativeY+height)
+	area11 := NewRectangleArea(width, height, relativeX+width, relativeY+height)
+	area12 := NewRectangleArea(width, height, relativeX+width*2, relativeY+height)
+	area20 := NewRectangleArea(width, height, relativeX, relativeY+height*2)
+	area21 := NewRectangleArea(width, height, relativeX+width, relativeY+height*2)
+	area22 := NewRectangleArea(width, height, relativeX+width*2, relativeY+height*2)
+
+	return [][]RectangleArea{
+		{area00, area01, area02},
+		{area10, area11, area12},
+		{area20, area21, area22},
 	}
 }
---
+++ adversarial-search/tictactoe.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"math"
+	"slices"
+)
+
+const X string = "X"
+const O string = "O"
+
+func initialState() [][]string {
+	return [][]string{
+		{"", "", ""},
+		{"", "", ""},
+		{"", "", ""},
+	}
+}
+
+func player(board [][]string) string {
+	moves := 0
+	for _, row := range board {
+		for _, cel := range row {
+			if cel != "" {
+				moves++
+			}
+		}
+	}
+
+	if moves%2 == 0 {
+		return X
+	}
+	return O
+}
+
+func actions(board [][]string) [][]int {
+	actions := [][]int{}
+	for i, row := range board {
+		for j, cel := range row {
+			if cel == "" {
+				actions = append(actions, []int{i, j})
+			}
+		}
+	}
+	slices.Reverse(actions)
+	return actions
+}
+
+func result(board [][]string, action []int) [][]string {
+	result := initialState()
+
+	for i, row := range board {
+		for j, cel := range row {
+			result[i][j] = cel
+		}
+	}
+
+	result[action[0]][action[1]] = player(board)
+	return result
+}
+
+func winner(board [][]string) string {
+
+	// check rows
+	if board[0][0] != "" && board[0][0] == board[0][1] && board[0][1] == board[0][2] {
+		return board[0][0]
+	} else if board[1][0] != "" && board[1][0] == board[1][1] && board[1][1] == board[1][2] {
+		return board[1][0]
+	} else if board[2][0] != "" && board[2][0] == board[2][1] && board[2][1] == board[2][2] {
+		return board[2][0]
+	}
+
+	// check collumns
+	if board[0][0] != "" && board[0][0] == board[1][0] && board[1][0] == board[2][0] {
+		return board[0][0]
+	} else if board[0][1] != "" && board[0][1] == board[1][1] && board[1][1] == board[2][1] {
+		return board[0][1]
+	} else if board[0][2] != "" && board[0][2] == board[1][2] && board[1][2] == board[2][2] {
+		return board[0][2]
+	}
+
+	// check diagonals
+	if board[0][0] != "" && board[0][0] == board[1][1] && board[1][1] == board[2][2] {
+		return board[0][0]
+	} else if board[0][2] != "" && board[0][2] == board[1][1] && board[1][1] == board[2][0] {
+		return board[0][2]
+	}
+
+	return ""
+}
+
+func terminal(board [][]string) bool {
+	if winner(board) != "" {
+		return true
+	}
+
+	if len(actions(board)) == 0 {
+		return true
+	}
+
+	return false
+}
+
+func utility(board [][]string) int {
+	w := winner(board)
+	if w == X {
+		return 1
+	} else if w == O {
+		return -1
+	} else {
+		return 0
+	}
+}
+
+func minimax(board [][]string) []int {
+	if terminal(board) {
+		return []int{}
+	}
+
+	player := player(board)
+	actions := actions(board)
+
+	bestAction := []int{}
+	var bestScore *int = nil
+
+	for _, action := range actions {
+		if player == X {
+			score := minValue(result(board, action), math.MinInt)
+			if bestScore == nil || score > *bestScore {
+				bestScore = &score
+				bestAction = action
+			}
+		} else if player == O {
+			score := maxValue(result(board, action), math.MaxInt)
+			if bestScore == nil || score < *bestScore {
+				bestScore = &score
+				bestAction = action
+			}
+		}
+	}
+
+	return bestAction
+}
+
+func maxValue(board [][]string, highestCurrentValue int) int {
+	if terminal(board) {
+		return utility(board)
+	}
+
+	lowestPossibleValue := math.MinInt
+	for _, action := range actions(board) {
+		lowerValue := minValue(result(board, action), lowestPossibleValue)
+		if lowerValue > highestCurrentValue {
+			return lowerValue
+		}
+		lowestPossibleValue = max(lowestPossibleValue, lowerValue)
+	}
+
+	return lowestPossibleValue
+}
+
+func minValue(board [][]string, lowestCurrentValue int) int {
+	if terminal(board) {
+		return utility(board)
+	}
+
+	highestPossibleValue := math.MaxInt
+	for _, action := range actions(board) {
+		higherValue := maxValue(result(board, action), highestPossibleValue)
+		if higherValue < lowestCurrentValue {
+			return higherValue
+		}
+		highestPossibleValue = min(highestPossibleValue, higherValue)
+	}
+
+	return highestPossibleValue
+}
